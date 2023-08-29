fd5a1d0f55e30f4ebad9ff13ae053379a3f5b900
Gregory Lindsey <gclindsey@gmail.com>
2023-08-28 17:01:54 -0700

    configure server

--- app/src/config.sh
+++ app/src/config.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -xe
 
-# Install Docker and Docker Compose
+# Install Dependencies
 yum update -y
 yum install -y docker git
 service docker start
@@ -10,8 +10,15 @@ curl -L https://github.com/docker/compose/releases/latest/download/docker-compos
 chmod +x /usr/local/bin/docker-compose
 docker-compose version
 
-# # Application specific configuration
-# mkdir -p /opt/app /opt/data/opensearch-data1 /opt/data/opensearch-data2
-# chown -R ec2-user:ec2-user /opt/app /opt/data
-# git clone https://github.com/chrisammon3000/building-data-pipelines-with-apache-kafka.git /opt/app
-# cd /opt/app && docker-compose up -d
+# Application specific configuration
+# https://opensearch.org/docs/latest/install-and-configure/install-opensearch/docker/#important-host-settings
+swapoff -a
+echo "vm.max_map_count=262144" >> /etc/sysctl.conf
+sysctl -p
+echo "Checking vm.max_map_count: $(cat /proc/sys/vm/max_map_count)"
+
+# Deploy services
+mkdir -p /opt/app /opt/data/opensearch-data1 /opt/data/opensearch-data2
+chown -R ec2-user:ec2-user /opt/app /opt/data
+git clone https://github.com/chrisammon3000/building-data-pipelines-with-apache-kafka.git /opt/app
+cd /opt/app && docker-compose up -d
